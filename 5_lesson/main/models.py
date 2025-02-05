6bde64a004fb12922d947cdfc398a532792859ef
Azimboyev-F7 <azimboyevfaxriyor.official@gmail.com>
2025-02-05 16:38:15 +0500

    odified

--- 5_lesson/main/models.py
+++ 5_lesson/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Workers(models.Model):
+    name = models.CharField(max_length=221)
+    second_name = models.CharField(max_length=221)
+    email = models.EmailField(max_length=221)
+    phone = models.CharField(max_length=221)
+    datetime = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- 5_lesson/main/views.py
+++ 5_lesson/main/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from main.models import Workers
+
+from main.models import Workers
+
 
 # Create your views here.
 
 def index(request):
+    workers = Workers.objects.all()
+    Workers.objects.get(id=1).delete()
+        # name="Faxriyorrr",
+        # second_name="Azimov",
+        # email="fazogir@gmail.com",
+        # phone=+998903585202,
+
     context = {}
     return render(request, 'index.html', context)
+
+
