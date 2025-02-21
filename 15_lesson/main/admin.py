a31f4ea817681861a884f6c3ddb1514306945b31
Azimboyev-F7 <azimboyevfaxriyor.official@gmail.com>
2025-02-21 18:54:56 +0500

    odified

--- 15_lesson/main/admin.py
+++ 15_lesson/main/admin.py
@@ -1,6 +1,13 @@
 from symtable import Class
-
+from .models import New
 from django.contrib import admin
 
 # Register your models here.
 
+class NewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('title', )
+    search_fields = ('id', 'title')
+    list_filter = ('title', )
+
+admin.site.register(New, NewAdmin)
--- 15_lesson/main/models.py
+++ 15_lesson/main/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class New:
+class New(models.Model):
     title = models.CharField(max_length=221)
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.title
