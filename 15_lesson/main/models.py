b4fc114d8f5d48303b8fff446fe5651d707c2475
Azimboyev-F7 <azimboyevfaxriyor.official@gmail.com>
2025-02-22 18:25:31 +0500

    modified

--- 15_lesson/main/models.py
+++ 15_lesson/main/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class New(models.Model):
+    image = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=221)
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
