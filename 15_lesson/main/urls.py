09617f6fdd9e1616ac49c29d918a1e77596af29e
Azimboyev-F7 <azimboyevfaxriyor.official@gmail.com>
2025-02-22 18:49:26 +0500

    designed

--- 15_lesson/main/urls.py
+++ 15_lesson/main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import *
 
 app_name = 'main'
 
 urlpatterns = [
     path('', index, name="index"),
+    path('article/', article, name='article'),
+    path('article/<int:pk>/', article, name='article'),
 ]
--- 15_lesson/main/views.py
+++ 15_lesson/main/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
+from .models import New
 
 # Create your views here.
 
 
 def index(request):
+    news = New.objects.all()
     context = {
-
+        'news': news
     }
-    return render(request, 'index.html', context)
+    return render(request, 'article/index.html', context)
+
+def article(request):
+    news = New.objects.all()
+    context = {
+        "news": news,
+    }
+    return render(request, 'article/articles.html', context)
+
+def detail(request):
+    news = New.objects.get(pk=id)
+    context = {
+        'id': news.id,
+    }
+    return render(request, 'article/detail.html', context)
