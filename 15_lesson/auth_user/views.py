069edf5a932f475e8e3a99b8b3780797fb318836
Azimboyev-F7 <azimboyevfaxriyor.official@gmail.com>
2025-03-12 16:45:19 +0500

    auth_designed

--- 15_lesson/auth_user/views.py
+++ 15_lesson/auth_user/views.py
@@ -6,6 +6,7 @@ from django.contrib import auth
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
+from .forms import MyUserCreationForm
 
 def login_view(request):
 
@@ -35,3 +36,18 @@ def logout_view(request):
     context = {}
 
     return render(request, 'auth/logout.html', context)
+
+def register_view(request):
+    form = MyUserCreationForm()
+    if request.method == 'POST':
+        form = MyUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'You are now registered')
+            return redirect('auth_user:login')
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'auth/register.html', context)
