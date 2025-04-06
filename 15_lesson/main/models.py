49080ea54abc0e60e7ff2db62304ad50b7df6882
Azimboyev-F7 <azimboyevfaxriyor.official@gmail.com>
2025-04-06 22:28:11 +0500

    modified

--- 15_lesson/main/models.py
+++ 15_lesson/main/models.py
@@ -1,13 +1,24 @@
 from django.db import models
+from django.utils.text import slugify
+from django.db.models.signals import post_save, pre_save
 
 # Create your models here.
 
 class New(models.Model):
     image = models.ImageField(upload_to='images/',null=True,blank=True)
     title = models.CharField(max_length=221)
+    slug = models.SlugField()
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+    
+
+
+def pre_save_new_receiver(sender, instance, *args, **kwargs):
+    instance.slug = slugify(instance.title)
+    if New.objects.filter(slug=slugify(instance.title)).exclude(id=instance.id).exists():
+        import uuid
+        instance.slug += f"{slugify(instance.title)}-{uuid.uuid4()}"
